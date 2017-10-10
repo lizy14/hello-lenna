64f9857c3c77547ada12c3d95e03df64ff8733d7
Li Zhaoyang <lizy14@yeah.net>
2017-10-10 17:51:32 +0800

    crop: rectangle with mouse

--- mainwindow.py
+++ mainwindow.py
@@ -1,6 +1,8 @@
 from PyQt5.uic import loadUi
-from PyQt5.QtWidgets import QMainWindow, QFileDialog, QGraphicsScene
-from PyQt5.QtCore import pyqtSlot, QRectF
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QGraphicsScene, QWidget
+from PyQt5.QtCore import pyqtSlot, QRectF, QPointF, QEvent, Qt
+from PyQt5.QtGui import QPen, QColor
+import PyQt5
 import cv233
 import cv233io
 import numpy
@@ -9,14 +11,57 @@ import numpy
 class MyMainWindow(QMainWindow):
     img = None
 
+    crop_rect_graphics_item = None
+    crop_start = None
+    crop_end = None
+    crop_mouse_down = False
+
     def __init__(self):
         super(MyMainWindow, self).__init__()
         loadUi('mainwindow.ui', self)
+
+        self.graphicsView.viewport().installEventFilter(self)
+
         self.progressBar.hide()
         MyMainWindow.showMaximized(self)
 
         self.img = cv233io.load('lenna.tif')
         self.paint(self.img)
+    
+    def eventFilter(self, source, event):
+        if source is self.graphicsView.viewport():
+
+            if event.type() == QEvent.MouseMove:
+                def get_crop_rect(start, end):
+                    xmin = min(start.x(), end.x())
+                    xmax = max(start.x(), end.x())
+                    ymin = min(start.y(), end.y())
+                    ymax = max(start.y(), end.y())
+                    return QRectF(QPointF(xmin, ymin), QPointF(xmax, ymax))
+
+                if self.crop_mouse_down:
+                    pos = self.graphicsView.mapToScene(event.pos())
+                    self.crop_end = pos
+                    scene = self.graphicsView.scene()
+                    scene.removeItem(self.crop_rect_graphics_item)
+                    self.crop_rect_graphics_item = scene.addRect(
+                        get_crop_rect(self.crop_start, self.crop_end), 
+                        QPen(Qt.green, 3, Qt.DashLine, Qt.SquareCap))
+
+            elif event.type() == QEvent.MouseButtonPress:
+                self.crop_mouse_down = True
+                pos = self.graphicsView.mapToScene(event.pos())
+                self.crop_start = pos
+
+            elif event.type() == QEvent.MouseButtonRelease:
+                self.crop_mouse_down = False
+                pos = self.graphicsView.mapToScene(event.pos())
+                if pos == self.crop_start:
+                    scene = self.graphicsView.scene()
+                    scene.removeItem(self.crop_rect_graphics_item)
+
+
+        return QWidget.eventFilter(self, source, event)
 
     # I/O helpers
 
