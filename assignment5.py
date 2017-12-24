93a71f76ac518d9742b52071ab71e4a9ebd16239
Li Zhaoyang <lizy14@yeah.net>
2017-12-25 00:30:09 +0800

    mask on spectrum operations

--- assignment5.py
+++ assignment5.py
@@ -13,7 +13,25 @@ def ifft(spectrum):
     return np.fft.ifft2(np.fft.ifftshift(spectrum)).real
 
 
-def freqBlurChannel(channel, method, param):
+def freqChannel(channel, direction, method, param):
+
+    if method == 'Ideal':
+        r = param
+        if direction == 'Sharpen':
+            mask = lambda x, y: .1 + (x*x + y*y >= r*r)
+        else:
+            mask = lambda x, y: x*x + y*y <= r*r
+    elif method == 'Gaussian':
+        sigma = param
+        if direction == 'Sharpen':
+            mask = lambda x, y: .1 + 1 - np.exp(-(x**2 + y**2) / (2. * sigma**2))
+        else:
+            mask = lambda x, y: np.exp(-(x**2 + y**2) / (2. * sigma**2))
+
+    return freqApplyMaskChannel(channel, mask)
+
+
+def freqApplyMaskChannel(channel, maskFunction):
     spectrum = fft(channel)
 
     w = spectrum.shape[0]
@@ -23,42 +41,16 @@ def freqBlurChannel(channel, method, param):
         -w // 2 : w // 2
     ]
 
-    if method == 'Ideal':
-        r = param
-        mask = x*x + y*y <= r*r
-    elif method == 'Gaussian':
-        sigma = param
-        mask = np.exp(-(x**2 + y**2) / (2. * sigma**2))
-    
+    mask = maskFunction(x, y)
+
     spectrum = np.multiply(spectrum, mask)
-    return ifft(spectrum)
-
-
-def freqSharpenChannel(channel, method, param):
-    spectrum = fft(channel)
-
-    w = spectrum.shape[0]
-    h = spectrum.shape[1]
-    y, x = np.ogrid[
-        -h // 2 : h // 2, 
-        -w // 2 : w // 2
-    ]
-
-    if method == 'Ideal':
-        r = param
-        mask = x*x + y*y >= r*r
-    elif method == 'Gaussian':
-        sigma = param
-        mask = 1 - np.exp(-(x**2 + y**2) / (2. * sigma**2))
-
-    spectrum = np.multiply(spectrum, mask + .1)
     return ifft(spectrum)
 
 @transformation
 def freqBlur(img, method='Gaussian', param=20):
-    return clip_to_byte(apply_channelwise(img, lambda c: freqBlurChannel(c, method, param)))
+    return clip_to_byte(apply_channelwise(img, lambda c: freqChannel(c, 'Blur', method, param)))
 
 @transformation
 def freqSharpen(img, method='Gaussian', param=10):
-    return clip_to_byte(apply_channelwise(img, lambda c: freqSharpenChannel(c, method, param)))
+    return clip_to_byte(apply_channelwise(img, lambda c: freqChannel(c, 'Sharpen', method, param)))
 
--- assignment6.py
+++ assignment6.py
@@ -5,11 +5,11 @@ assignment 6: artistic effect
 import numpy as np
 import cv2
 from utils import *
-from assignment5 import freqBlurChannel
+from assignment5 import freqChannel
 
 @transformation
 def pencil(img, param):
     img_gray = rgb_to_gray(img)
-    img_blur = freqBlurChannel(img_gray, 'Gaussian', param)
+    img_blur = freqChannel(img_gray, 'Blur', 'Gaussian', param)
     img_blend = clip_to_byte(img_gray * 256. / img_blur)
     return gray_to_rgb(img_blend)
